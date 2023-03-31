df85fb70b90de1c779f7efa7967aa45fe27d8a0b
Ariana Torres <at5322931@gmail.com>
2023-03-31 16:00:38 -0600

    fin

--- src/categoria/dto/create-categoria.dto.ts
+++ src/categoria/dto/create-categoria.dto.ts
@@ -1,11 +1,3 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
-export class CreateCategoriaDto {
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-
-    @IsString()
-    @IsNotEmpty()
-    detalle: string;
-}
+export class CreateCategoriaDto {}
